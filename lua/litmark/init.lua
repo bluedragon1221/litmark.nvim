a87b616903bc7690c9c3f82aa7bc0d2db44694e1
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2023-04-30 06:36:55 -0500

    move around `scratch()` and `source()` functions

--- lua/litmark/init.lua
+++ lua/litmark/init.lua
@@ -4,7 +4,7 @@ local vim = vim -- supress "Undefined global" errors
 
 local M = {}
 
-function M.remove_non_code_lines()
+local function remove_non_code_lines()
     local in_code_block = false
     local new_lines = {}
     for _, line in ipairs(vim.api.nvim_buf_get_lines(0,0,-1,false)) do
@@ -25,6 +25,24 @@ function M.remove_non_code_lines()
     end
 
     return final_lines
+end
+
+
+function M.scratch()
+    local bufnr = vim.api.nvim_create_buf(false,true)
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, remove_non_code_lines())
+    vim.api.nvim_command("belowright split")
+    vim.api.nvim_command("buffer " .. bufnr)
+    vim.cmd [[ set ft=lua ]]
+end
+
+function M.source()
+    local success, error_message = pcall(load(table.concat(remove_non_code_lines(), '\n')))
+
+    -- Check for errors
+    if not success then
+        print("Error:", error_message)
+    end
 end
 
 return M
--- plugin/litmark.lua
+++ plugin/litmark.lua
@@ -1,22 +1,5 @@
 local litmark = require('litmark')
 local vim = vim -- Supress "Undefined global" errors
 
-local function scratch(lines)
-    local bufnr = vim.api.nvim_create_buf(false,true)
-    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
-    vim.api.nvim_command("belowright split")
-    vim.api.nvim_command("buffer " .. bufnr)
-    vim.cmd [[ set ft=lua ]]
-end
-
-local function source(lua_code)
-    local success, error_message = pcall(load(table.concat(lua_code, '\n')))
-
-    -- Check for errors
-    if not success then
-        print("Error:", error_message)
-    end
-end
-
-vim.api.nvim_create_user_command('LMshow',  function() scratch(litmark.remove_non_code_lines()) end, {})
-vim.api.nvim_create_user_command('LMsource', function() source(litmark.remove_non_code_lines()) end, {})
+vim.api.nvim_create_user_command('LMshow',  function() litmark.scratch() end, {})
+vim.api.nvim_create_user_command('LMsource', function() litmark.source() end, {})
